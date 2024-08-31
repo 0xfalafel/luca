6b429303921e65917f5f5746dd6168a698155638
Olivier Lasne <lasneolivier@gmail.com>
2024-08-31 17:58:51 +0200

    Move the text input component to a newfile

---
+++ src/input_pane.rs
@@ -0,0 +1,58 @@
+use gtk::prelude::{WidgetExt, TextBufferExt, TextViewExt};
+use relm4::{gtk, ComponentParts, ComponentSender, SimpleComponent};
+
+// Input component
+
+pub struct LucaInput {
+    text: String,
+    text_buffer: gtk::TextBuffer
+}
+
+#[derive(Debug)]
+pub enum Msg {
+    TextChanged(String)
+}
+
+#[relm4::component(pub)]
+impl SimpleComponent for LucaInput {
+    type Init = String;
+    type Input = Msg;
+    type Output = ();
+
+    view! {
+        gtk::TextView {
+            set_margin_start: 20,
+            set_buffer: Some(&model.text_buffer)
+        },
+    }
+
+    fn init(
+        text: Self::Init,
+        root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let text_buffer = gtk::TextBuffer::new(None);
+        text_buffer.set_text(&text);
+
+        let text_buffer_clone = text_buffer.clone();
+        text_buffer_clone.connect_changed(move |text_buffer| {
+            let start_iter = text_buffer.start_iter();
+            let end_iter = text_buffer.end_iter();
+            let text = text_buffer.text(&start_iter, &end_iter, false);
+            sender.input(Msg::TextChanged(text.to_string()));
+        });
+
+        let model = LucaInput {text, text_buffer};
+        let widgets = view_output!();
+        ComponentParts {model, widgets}
+    }
+
+    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        match msg {
+            Msg::TextChanged(text) => {
+                self.text = text;
+                println!("Text changed: {}", self.text);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,10 @@
 use gtk::{gdk, glib, glib::clone};
-use gtk::prelude::{GtkWindowExt, OrientableExt, WidgetExt, TextBufferExt, TextViewExt};
+use gtk::prelude::{GtkWindowExt, OrientableExt, WidgetExt};
 use relm4::{gtk, Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmApp, SimpleComponent};
 use granite::prelude::SettingsExt;
 
-// Input component
-
-struct LucaInput {
-    text: String,
-    text_buffer: gtk::TextBuffer
-}
-
-#[derive(Debug)]
-enum Msg {
-    TextChanged(String)
-}
-
-#[relm4::component]
-impl SimpleComponent for LucaInput {
-    type Init = String;
-    type Input = Msg;
-    type Output = ();
-
-    view! {
-        gtk::TextView {
-            set_margin_start: 20,
-            set_buffer: Some(&model.text_buffer)
-        }
-    }
-
-    fn init(
-        text: Self::Init,
-        root: Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let text_buffer = gtk::TextBuffer::new(None);
-        text_buffer.set_text(&text);
-
-        let text_buffer_clone = text_buffer.clone();
-        text_buffer_clone.connect_changed(move |text_buffer| {
-            let start_iter = text_buffer.start_iter();
-            let end_iter = text_buffer.end_iter();
-            let text = text_buffer.text(&start_iter, &end_iter, false);
-            sender.input(Msg::TextChanged(text.to_string()));
-        });
-
-        let model = LucaInput {text, text_buffer};
-        let widgets = view_output!();
-        ComponentParts {model, widgets}
-    }
-
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
-        match msg {
-            Msg::TextChanged(text) => {
-                self.text = text;
-                println!("Text changed: {}", self.text);
-            }
-        }
-    }
-}
+mod input_pane;
+use input_pane::LucaInput;
 
 // Application model
 
