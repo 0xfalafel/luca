37d263d5523fd8620da767e33b0c4a8ec820a36b
Lasne Olivier <lasneolivier@gmail.com>
2025-03-21 12:09:31 +0100

    started implementing Add trait for Value

--- src/value.rs
+++ src/value.rs
@@ -1,8 +1,15 @@
+use std::ops::Add;
+
 use num_rational::BigRational;
+
+#[derive(Debug, Clone, PartialEq)]
+
 enum Unit {
     Euros,
     Dollars
 }
+
+#[derive(Debug, Clone, PartialEq)]
 
 pub struct Value {
     number: BigRational,
@@ -15,5 +22,23 @@ impl Value {
             number: number,
             unit: vec![]
         }
+    }
+}
+
+pub enum CalculationError {
+    IncompatileTypes
+}
+
+impl Add<Value> for Value {
+    type Output = Result<Value, CalculationError>;
+
+    fn add (self, rhs: Value) -> Self::Output {
+
+
+        // If the types are compatible, procced with the addition
+        Ok(Value {
+            number: self.number + rhs.number,
+            unit: self.unit
+        })
     }
 }
