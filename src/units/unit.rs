c7b763dbabb975b785d274c7f44145e581a29d3c
Olivier Lasne <lasneolivier@gmail.com>
2025-05-12 14:52:53 +0200

    added a conversion from centimeter to meter

--- src/units/unit.rs
+++ src/units/unit.rs
@@ -19,10 +19,29 @@ pub struct Unit {
 }
 
 impl Unit {
+    #[allow(unused)]
+    fn is_base_unit(&self) -> bool {
+        self.kind == UnitKind::Base
+    }
+
+    /// Return the name of the base, and conversion factor
+    fn base_and_factor(&self) -> (&CanonicalName, f64) {
+        match &self.kind {
+            UnitKind::Derived(conversion_factor, base_unit) => (&base_unit.name, *conversion_factor),
+            UnitKind::Base => (&self.name, 1.0)
+        }
+    }
+
     pub fn conversion_factor(&self, dest_unit: Unit) -> Option<f64> {
+        let (base_unit_self, convertion_factor_self) = self.base_and_factor();
+        let (base_unit_other, convertion_factor_other) = dest_unit.base_and_factor();
 
+        // We can only do conversion if units are derived from each other (aka: cm to m)
+        if base_unit_self != base_unit_other {
+            return None
+        }
 
-        Some(10.0)
+        Some(convertion_factor_self / convertion_factor_other)
     }
 }
 
@@ -48,5 +67,24 @@ impl Unit {
 
     pub fn dollar() -> Unit {
         Unit { symbol: "$".into(), name: "dollar".into(), kind: UnitKind::Base }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn m_to_cm() {
+        let m = Unit::meter();
+        let cm = Unit::centimeter();
+        assert_eq!(Some(100.0), m.conversion_factor(cm));
+    }
+
+    #[test]
+    fn cm_to_m() {
+        let m = Unit::meter();
+        let cm = Unit::centimeter();
+        assert_eq!(Some(0.01), cm.conversion_factor(m));
     }
 }
