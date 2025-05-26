01ba33f7b395af573787679bdd539c1039f70815
Lasne Olivier <lasneolivier@gmail.com>
2025-05-26 12:34:15 +0200

    fix an issue where `m² ` was not parsed a m²

--- tests/interpreter.rs
+++ tests/interpreter.rs
@@ -371,3 +371,31 @@ fn m_per_s_to_km_per_h() {
         units)
     ));
 }
+
+#[test]
+fn two_vars() {
+    let result = make_interpreter("1m² ", None).interpret();
+    assert_eq!(result, Ok(Value::new_with_units(
+        Number::one(),
+        ComposedUnit::square_meters())
+    ));
+}
+
+
+#[test]
+fn one_square_meter() {
+    let result = make_interpreter("1m² ", None).interpret();
+    assert_eq!(result, Ok(Value::new_with_units(
+        Number::one(),
+        ComposedUnit::square_meters())
+    ));
+}
+
+#[test]
+fn square_meter() {
+    let result = make_interpreter("1 m² * 2", None).interpret();
+    assert_eq!(result, Ok(Value::new_with_units(
+        Number::from_i64(2).unwrap(),
+        ComposedUnit::square_meters())
+    ));
+}
