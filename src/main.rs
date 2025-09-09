b40014011deec75d167d54a84a88725b1cf54f19
Olivier Lasne <lasneolivier@gmail.com>
2025-09-09 14:51:56 +0200

    created a gtk menu

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use gtk::prelude::{GtkWindowExt, OrientableExt, WidgetExt};
 use relm4::gtk::gio;
 use log::{error, info};
 use relm4::gtk::prelude::{AdjustmentExt, ButtonExt};
+use relm4::gtk::gio;
 use relm4::{gtk, Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmApp, SimpleComponent};
 use granite::prelude::SettingsExt;
 
